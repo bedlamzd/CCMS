f2a403a4882ccfb6fb28f977032a89cdd5b3432c
bedlamzd <bedlamzd@gmail.com>
2020-03-03 00:42:37 +0300

    Старые версии симулинк

---
+++ export_to_old.m
@@ -0,0 +1,18 @@
+files = ["C:\Users\bedla\Studying\CCMS\L2\src\p_isled.slx"...
+         "C:\Users\bedla\Studying\CCMS\L2\src\p_isled_zap.slx"...
+         "C:\Users\bedla\Studying\CCMS\L3\src\i_isled.slx"...
+         "C:\Users\bedla\Studying\CCMS\L3\src\i_isled_zap.slx"];
+versions = ["R2018b"...
+            "R2017b"];
+for file = files
+    [filepath,name,ext] = fileparts(file);
+    open(file);
+    if ~exist(filepath + '\old\', 'dir')
+        mkdir(filepath + '\old\')
+    end
+    for v = versions
+        Simulink.exportToVersion(name, filepath + '\old\' + name + '_' + v, v)
+    end
+    close_system
+end
+
